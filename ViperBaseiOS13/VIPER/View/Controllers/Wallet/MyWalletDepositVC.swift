2982d90933179412ab3d2665871dbb1fc3e426bb
Asif <asif.khan@appinventiv.com>
2021-05-13 11:09:23 +0530

    # Updated Code + Deposit screen ui

--- ViperBaseiOS13/VIPER/View/Controllers/Wallet/MyWalletDepositVC.swift
+++ ViperBaseiOS13/VIPER/View/Controllers/Wallet/MyWalletDepositVC.swift
@@ -14,13 +14,21 @@ class MyWalletDepositVC: UIViewController {
     
     // MARK: - IBOutlets
     //===========================
-    @IBOutlet weak var currencyTxtField: DropDown!
+    @IBOutlet weak var addAmtLbl: UILabel!
+    @IBOutlet weak var depositToWalletLbl: UILabel!
+    @IBOutlet weak var currencyTxtField: UITextField!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var amtTxtField: UITextField!
     @IBOutlet weak var proceedBtn: UIButton!
     @IBOutlet weak var dataContainerView: UIView!
     @IBOutlet weak var currencyControl: UISegmentedControl!
     // MARK: - Variables
     //===========================
-    
+    var selectedCurrencyType = "ETC"
+    var  buttonView = UIButton()
+    private lazy var loader  : UIView = {
+           return createActivityIndicator(self.view)
+       }()
     // MARK: - Lifecycle
     //===========================
     override func viewDidLoad() {
@@ -30,6 +38,7 @@ class MyWalletDepositVC: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        self.proceedBtn.setCornerRadius()
         self.dataContainerView.roundCorners([.layerMinXMinYCorner, .layerMaxXMinYCorner], radius: 15.0)
     }
     
@@ -49,9 +58,68 @@ class MyWalletDepositVC: UIViewController {
 
 // MARK: - Extension For Functions
 //===========================
-extension MyWalletDepositVC {
+extension MyWalletDepositVC: UITextFieldDelegate {
     
     private func initialSetup() {
-        
+        self.setFont()
+        self.currencyTxtField.delegate = self
+        self.amtTxtField.keyboardType = .numberPad
+        currencyControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        buttonView.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        currencyTxtField.setButtonToRightView(btn: buttonView, selectedImage: #imageLiteral(resourceName: "icDropdown"), normalImage: #imageLiteral(resourceName: "icDropdown"), size: CGSize(width: 20, height: 20))
+        currencyTxtField.text =  self.selectedCurrencyType
+    }
+    
+    private func setFont(){
+//        currencyControl.setTitleTextAttributes([NSAttributedString.Key.font: isDeviceIPad ? 20 : 15], for: .normal)
+        self.currencyTxtField.font =  isDeviceIPad ? .setCustomFont(name: .medium, size: .x20) : .setCustomFont(name: .medium, size: .x15)
+        self.amtTxtField.font =  isDeviceIPad ? .setCustomFont(name: .regular, size: .x28) : .setCustomFont(name: .regular, size: .x24)
+        self.depositToWalletLbl.font =  isDeviceIPad ? .setCustomFont(name: .regular, size: .x20) : .setCustomFont(name: .regular, size: .x16)
+        self.addAmtLbl.font =  isDeviceIPad ? .setCustomFont(name: .semiBold, size: .x18) : .setCustomFont(name: .semiBold, size: .x14)
+        self.descLbl.font =  isDeviceIPad ? .setCustomFont(name: .regular, size: .x16) : .setCustomFont(name: .regular, size: .x12)
+        self.proceedBtn.titleLabel?.font =  isDeviceIPad ? .setCustomFont(name: .bold, size: .x20) : .setCustomFont(name: .bold, size: .x16)
+    }
+    
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            currencyTxtField.setButtonToRightView(btn: buttonView, selectedImage: #imageLiteral(resourceName: "icDropdown"), normalImage: #imageLiteral(resourceName: "icDropdown"), size: CGSize(width: 20, height: 20))
+            currencyTxtField.text =  self.selectedCurrencyType
+            currencyTxtField.isUserInteractionEnabled = true
+        } else {
+            currencyTxtField.rightView = nil
+            currencyTxtField.inputView = nil
+            currencyTxtField.text = " Dollar (USD)"
+            currencyTxtField.isUserInteractionEnabled = false
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        _ = textField.text?.byRemovingLeadingTrailingWhiteSpaces ?? ""
+        self.view.endEditing(true)
+        guard let vc = Router.main.instantiateViewController(withIdentifier: Storyboard.Ids.ProductSortVC) as? ProductSortVC else { return }
+        vc.delegate = self
+        vc.usingForSort = .filter
+        vc.sortArray = [("ETC",false),("BITCOIN",false)]
+        vc.sortTypeApplied = self.selectedCurrencyType
+        self.present(vc, animated: true, completion: nil)
+    }
+      
+      func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+          if range.location == 0 && (string == " ") {
+              return false
+          }
+          return true
+      }
+    
+}
+
+
+//MARK:- Sorting
+//==============
+extension MyWalletDepositVC: ProductSortVCDelegate{
+    
+    func sortingApplied(sortType: String){
+        self.selectedCurrencyType = sortType
+        currencyTxtField.text =  self.selectedCurrencyType
     }
 }
